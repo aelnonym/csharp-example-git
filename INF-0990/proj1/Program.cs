48211fe408d5d97da38c9f9df8deda8517f580d2
Aelnonym <aelnonym@gmail.com>
2022-08-27 18:33:26 -0300

    Added junk code

--- INF-0990/proj1/Program.cs
+++ INF-0990/proj1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int Max(int a, int b)
+{
+  return (a > b) ? a : b;
+}
+int a = 3;
+int b = 2;
+
+Max(a, b);   // 3
+Max(b, a);   // 3
+
+Console.WriteLine($"Max value between {a} and {b} is {Max(a, b)}");
